526019ee448ef44b52dc8519b498cebb4cdba2dd
eZ3KIEL29640 <yohann.cloarec@gmail.com>
2024-08-27 22:59:28 +0200

    Update SlopeV4.py

--- SlopeV4.py
+++ SlopeV4.py
@@ -106,8 +106,8 @@ class SlopeV4(IStrategy):
             (
                 (dataframe['volume_pct'] > self.volume_short.value) &
                 (dataframe['mid_di'] > -8) &
+                (dataframe['minus_di'] < dataframe['mid_di']) &
                 (dataframe['mid_di'] < dataframe['plus_di']) &
-                (dataframe['plus_di'] < dataframe['minus_di']) &
                 (dataframe['volume']     > 0)
             ),
         'enter_short'] = 1
